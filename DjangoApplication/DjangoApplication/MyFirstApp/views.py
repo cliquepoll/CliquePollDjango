033dc1430d1678ed57da0d7aa046ec78cb6d664d
Clique Poll <riqbal@live.com>
2013-04-04 07:33:57 +0000

    First Django App
    Django App

---
+++ DjangoApplication/DjangoApplication/MyFirstApp/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+def home(request):
+    return HttpResponse(render_to_string(
+                                        'index.html',
+                                        {'content': 'Hello World'}
+                                        ))
